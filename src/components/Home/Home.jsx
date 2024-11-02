f924b0e38aa9f03fcb5c0182d002ceee09488722
jayen123 <jayenhasan2003@gmail.com>
2024-11-03 00:24:43 +0600

    NavLink, useNavigation,useLocation,Navigate

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import { MagnifyingGlass } from "react-loader-spinner";
 
 export default function Home() {
+  const navigation = useNavigation();
+  const location = useLocation();
+  console.log(location);
+
   return (
     <div>
       <Navbar />
-      <Outlet />
+      {navigation.state === "loading" ? (
+        <MagnifyingGlass
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="magnifying-glass-loading"
+          wrapperStyle={{}}
+          wrapperClass="magnifying-glass-wrapper"
+          glassColor="#c0efff"
+          color="#e15b64"
+        />
+      ) : (
+        <Outlet />
+      )}
+
       <Footer />
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,25 @@
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav>
-        <ul className="flex gap-5">
-          <li>
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link to={"/users"}>Users</Link>
-          </li>
-          <li>
-            <Link to={"/posts"}>Posts</Link>
-          </li>
-          <li>
-            <Link to={"/about"}>About</Link>
-          </li>
-          <li>
-            <Link to={"/contact"}>Contact</Link>
-          </li>
-        </ul>
-  
+      <ul className="flex gap-5">
+        <li>
+          <NavLink className={'font-bold text-lg'} to={"/"}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink className={'font-bold text-lg'} to={"/users"}>Users</NavLink>
+        </li>
+        <li>
+          <NavLink className={'font-bold text-lg'} to={"/posts"}>Posts</NavLink>
+        </li>
+        <li>
+          <NavLink className={'font-bold text-lg'} to={"/about"}>About</NavLink>
+        </li>
+        <li>
+          <NavLink className={'font-bold text-lg'} to={"/contact"}>Contact</NavLink>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
